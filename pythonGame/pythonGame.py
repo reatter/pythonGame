47eda1ff88626a85bb7ee36cca434c153e75701a
Michael Lannert <michael.lannert@posteo.de>
2023-08-11 19:16:30 +0200

    pygame setup

--- pythonGame/pythonGame.py
+++ pythonGame/pythonGame.py
@@ -1,3 +1,9 @@
+# Am Anfang kann man Module laden und importieren
+# Das sind Programmteile, die von anderen programmiert wurden
+import pygame
+from pygame.locals import *
+
+
 # Das ist ein Kommentar
 
 # Python speaks english
@@ -7,3 +13,18 @@ print("Hallo Paul")
 for nummer in range(100):
     # das innere der Schleife ist eingerückt
     print("Zahl ", nummer)
+
+pygame.init() # pygame startet
+
+# Farben werden als RGB-Tupel gespeichert
+# ROT  GRÜN BLAU WERT aus 0-255
+BLUE = (0, 0, 255)
+
+# ein Screen zum Zeichnen des spiels
+screen = pygame.display.set_mode(size=(1920, 1080))
+
+# def definiert eine Funktion, die man immer wieder verwenden kann.
+def drawCircle():
+    pos=pygame.mouse.get_pos()
+    pygame.draw.circle(screen, BLUE, pos, 20) # Here <<<
+
