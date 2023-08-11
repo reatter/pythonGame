cc5a4d8db873a1626411b682a709c4b819026cf1
Michael Lannert <michael.lannert@posteo.de>
2023-08-11 18:52:06 +0200

    for-schleife

---
+++ pythonGame/pythonGame.py
@@ -0,0 +1,9 @@
+# Das ist ein Kommentar
+
+# Python speaks english
+print("Hallo Paul")
+
+# damit python etwas mehrmals macht, gibt es die for-Schleife, und Listen wir range(100)
+for nummer in range(100):
+    # das innere der Schleife ist eingerückt
+    print("Zahl ", nummer)
